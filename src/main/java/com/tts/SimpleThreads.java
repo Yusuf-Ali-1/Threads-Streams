7465764c95dbee0833ba9c5a8291bd6cdf4991b1
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-03 09:45:40 -0400

    first commit

--- src/main/java/com/tts/SimpleThreads.java
+++ src/main/java/com/tts/SimpleThreads.java
@@ -7,7 +7,7 @@ public class SimpleThreads {
     static void threadMessage(String message) {
         String threadName =
                 Thread.currentThread().getName();
-        System.out.format("%s: %s%n",
+        System.out.printf("%s: %s%n",
                 threadName,
                 message);
     }
@@ -22,9 +22,7 @@ public class SimpleThreads {
                     "A kid will eat ivy too"
             };
             try {
-                for (int i = 0;
-                     i < importantInfo.length;
-                     i++) {
+                for (int i = 0; i < importantInfo.length; i++) {
                     // Pause for 4 seconds
                     Thread.sleep(4000);
                     // Print a message
