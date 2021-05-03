50c6edab7fa58f4c50352693801c3f58648fd317
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-03 08:51:50 -0400

    first commit

--- src/main/java/com/tts/HelloThread.java
+++ src/main/java/com/tts/HelloThread.java
@@ -4,6 +4,7 @@ public class HelloThread extends Thread{
 
     @Override
     public void run(){
-        System.out.println("Hello from a custom thread class!");
+        System.out.println("Hello from a custom thread class! Here is the thread's id: "
+        + Thread.currentThread().getId());
     }
 }
