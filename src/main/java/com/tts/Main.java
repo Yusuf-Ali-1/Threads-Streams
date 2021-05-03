cb32ec437793167c9a938941f8f54cfda8734e51
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2021-05-03 08:26:52 -0400

    first commit

--- src/main/java/com/tts/Main.java
+++ src/main/java/com/tts/Main.java
@@ -3,6 +3,7 @@ package com.tts;
 public class Main {
     //this main method is known as our entry point
     // but in fact, it's also known as our main thread
+    // The main thread has the ability to create additional threads
     public static void main(String[] args) {
 
         System.out.println("Hello world, I'm in a main thread");
